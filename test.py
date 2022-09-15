7c070c63f4025f72475162b9be08a53462c2d8e0
Amar Bahadur KC <88132618+amarkc-01@users.noreply.github.com>
2022-09-15 12:17:29 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+# Capitol Maps: Find your way around D.C.!
+latitude=float(input("Insert your latitude: "))
+longitude=float(input("Insert your longitude: "))
+
+#converting  degree difference into miles
+difference_lat=(latitude-38.921669)*69
+diference_lon=(longitude--77.021361)*53
+#calculating the difference between places using pythagorean theorem 
+distance=diflat**2+diflon**2
+finalans=adis**0.5
+print("You are %.1f miles from Downing Hall"%finalans)
+
